32bc649aa6de7a97b610d576eefecbbfcbdff3ed
konstantin arzamastsev <volgavolga1995@yandex.ru>
2023-07-19 22:58:39 +0300

    видео

--- source/js/modules/video.js
+++ source/js/modules/video.js
@@ -1,3 +1,4 @@
+
 function findVideos() {
   let videos = document.querySelectorAll('.video');
 
@@ -24,11 +25,31 @@ function setupVideo(video) {
   video.classList.add('video--enabled');
 }
 
-function parseMediaURL(media){
-let regexp = ;
-let url = media.src;
-let match = url.match(regexp);
+function parseMediaURL(media) {
+  let regexp = /https:\/\/i\.ytimgi\.com\vi\/([a-zA-Z0-9_-]+)\/maxresdefault\.jpg/i;
+  let url = media.src;
+  let match = url.match(regexp);
 
-return match[1];
+  return match[1];
 
 }
+
+function createIframe(id) {
+  let iframe = document.createElement('iframe');
+
+  iframe.setAttribute('allowfullscreen', '');
+  iframe.setAttribute('allow', 'autoplay');
+  iframe.setAttribute('src', generateURL(id));
+  iframe.classList.add('video__media');
+
+  return iframe;
+
+}
+
+function generateURL(id) {
+  let query = '?rel=0&showinfo=0&autoplay=1';
+
+  return 'https://www.youtube.com/embed' + id + query;
+}
+
+findVideos();
