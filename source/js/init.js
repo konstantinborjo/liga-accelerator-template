307c968b64d064f0ff61109dd76118762d93ec41
konstantinborjo <112392438+konstantinborjo@users.noreply.github.com>
2023-09-05 14:39:23 +0300

    код

--- source/js/init.js
+++ source/js/init.js
@@ -1,59 +1,64 @@
-const Swiper('.swiper', {
-  loop: true,
-  spaceBetween: 40,
-  navigation: {
-    nextEl: '.swiper-button--next',
-    prevEl: '.swiper-button--prev',
-  },
-  breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 1,
-      slidesPerGroup: 1,
+const Swiper = window.Swiper;
+(() =>
+  new Swiper('.swiper', {
+    loop: true,
+    spaceBetween: 40,
+    navigation: {
+      nextEl: '.swiper-button--next',
+      prevEl: '.swiper-button--prev',
     },
-    // when window width is >= 768px
-    768: {
-      slidesPerView: 2,
-      slidesPerGroup: 2,
-      spaceBetween: 30,
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+      },
+      // when window width is >= 768px
+      768: {
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+        spaceBetween: 30,
 
-    },
+      },
 
-    1200: {
-      slidesPerView: 3,
-      slidesPerGroup: 3,
+      1200: {
+        slidesPerView: 3,
+        slidesPerGroup: 3,
+      },
+      // when window width is >= 1280px
+      1280: {
+        slidesPerView: 4,
+        slidesPerGroup: 4,
+      },
     },
-    // when window width is >= 1280px
-    1280: {
-      slidesPerView: 4,
-      slidesPerGroup: 4,
-    },
-  },
-})
+  })
+)();
 
-const Swiper('.comment__swiper', {
-  loop: true,
-  spaceBetween: 10,
-  navigation: {
-    nextEl: '.swiper-button--next',
-    prevEl: '.swiper-button--prev',
-  },
-  breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 1,
-      slidesPerGroup: 1,
+(() =>
+  new Swiper('.comment__swiper', {
+    loop: true,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: '.swiper-button--next',
+      prevEl: '.swiper-button--prev',
     },
-    // when window width is >= 768px
-    768: {
-      slidesPerView: 1,
-      slidesPerGroup: 1,
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+      },
+      // when window width is >= 768px
+      768: {
+        slidesPerView: 1,
+        slidesPerGroup: 1,
 
+      },
+      // when window width is >= 1280px
+      1280: {
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+      },
     },
-    // when window width is >= 1280px
-    1280: {
-      slidesPerView: 1,
-      slidesPerGroup: 1,
-    },
-  },
-});
+  })
+)();
